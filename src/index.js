c77a498140c8621f41502f146c8d984368f2f78c
Chris Stallcup <chris@Chriss-MacBook-Pro.local>
2022-01-24 15:36:36 -0600

    removed some unsed default react files and added components for my to use for portfolio page

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import PortfolioContainer from "./components/PortfolioContainer";
+import Footer from "./components/Footer";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function App() {
+  return (
+  <div>
+   <PortfolioContainer />
+   <Footer />
+  </div>
+  )
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+export default App;
